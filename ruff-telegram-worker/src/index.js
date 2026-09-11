fdf40417e791d3246261ac8b2cc8307b28c3b94e
Ruff Pup <woof@ruffthe.dog>
2026-09-11 12:16:14 -0400

    updated SEBB URL

--- ruff-telegram-worker/src/index.js
+++ ruff-telegram-worker/src/index.js
@@ -67,7 +67,7 @@ export default {
               black_and_blue: {
                 label: "Black & Blue",
                 emoji: "💙",
-                url: "https://ruffthe.dog/places/atlanta/atleagle/sebb_blackandblue/"
+                url: "https://ruffthe.dog/places/atlanta/atleagle/sebbblackandblue/"
               }
             }
           },
--- ruff-telegram-worker/src/version.generated.js
+++ ruff-telegram-worker/src/version.generated.js
@@ -1,2 +1,2 @@
 export const WORKER_VERSION = "1.1.0";
-export const DEPLOYED_AT = "2026-08-07T16:29:07.217Z";
+export const DEPLOYED_AT = "2026-09-11T16:10:49.159Z";
